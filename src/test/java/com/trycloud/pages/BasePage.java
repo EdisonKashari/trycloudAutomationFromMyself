efd70934435ed235f4da4a6575412526a1b352be
Edison Kashari <edisonkashari@hotmail.com>
2022-03-29 18:24:41 -0400

    First user story is done

--- src/test/java/com/trycloud/pages/BasePage.java
+++ src/test/java/com/trycloud/pages/BasePage.java
@@ -1,6 +1,8 @@
 package com.trycloud.pages;
 
 import com.trycloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class BasePage {
@@ -8,4 +10,17 @@ public class BasePage {
     public BasePage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
+
+    @FindBy(id = "user")
+    public WebElement user_name;
+
+
+    @FindBy(id = "password")
+    public WebElement password;
+
+    @FindBy(id = "submit-form")
+    public WebElement login_button;
+
+    @FindBy(xpath = "(//span[normalize-space(text()) = 'Dashboard'])[1]")
+    public WebElement dashboard;
 }
